cc64d4986a6d9f839119f40349c984f3ec98a2af
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2015-02-18 14:29:39 -0500

    Update chr_transform.sh

--- chr_transform.sh
+++ chr_transform.sh
@@ -2,25 +2,23 @@
 #$ -cwd
 
 # transform data into a longGWAS accepted .snps file
-
-FILE=$1
 CHR=$2
 
 #DIRECTORIES
 DIR=/myDir
 
 # remove first 6 columns
-cut -d " " -f 7- ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.raw > ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp
+cut -d " " -f 7- ${DIR}/myStudy-${CHR}.raw > ${DIR}/myStudy-${CHR}.tmp
 
 # remember header (SNP names)
-head -n 1 ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp > ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.head
+head -n 1 ${DIR}/myStudy-${CHR}.tmp > ${DIR}/myStudy-${CHR}.head
 
 # remove header
-sed -i "1d" ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp
+sed -i "1d" ${DIR}/myStudy-${CHR}.tmp
 
 # substitute 1 to 0.5, and 2 to 1
-sed -i "s/1/0.5/g" ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp
-sed -i "s/2/1/g" ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp
+sed -i "s/1/0.5/g" ${DIR}/myStudy-${CHR}.tmp
+sed -i "s/2/1/g" ${DIR}/myStudy-${CHR}.tmp
 
 # transpose the dataset
 awk '
@@ -38,7 +36,7 @@ END {
         }
         print str
     }
-}' ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp > ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.snps
+}' ${DIR}/myStudy-${CHR}.tmp > ${DIR}/myStudy-${CHR}.snps
 
 # remove temporary file
-rm ${DIR}/${FILE}/${FILE}-${CHR}-eur-qc.tmp
+rm ${DIR}/myStudy-${CHR}.tmp
