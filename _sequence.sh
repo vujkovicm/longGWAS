6a5286a901424e27e23d2f8787bf9207fe66774d
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2015-02-20 11:34:51 -0500

    Update _sequence.sh

--- _sequence.sh
+++ _sequence.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 #$ -cwd
 
+# (chr x 23)
 for i in {1..23}
 do
+    # export plink data to transform and make it longGWAS compatible 
     qsub chr_recodeA.sh $i
+    # make .snps file for longGWAS 
     qsub chr_transform.sh $i
 done
 
-qsub gen_tped2bed.sh
-qsub gen_gcta.sh
-R CMD BATCH run_longGWAS.R
-R CMD BATCH get_P.R
-cat.sh
+# merge chr into genome for kinship calculations
+qsub snp_chr2genome.sh
 
-# then perform a manhattan analysis
+# run kinship with GCTA (genome x 1)
+qsub run_gcta.sh
+
+# run longGWAS in R (chr x 23)
+R CMD BATCH run_longGWAS.R
+
+# transform longGWAS output to SNP-CHR-BP-P format (chr x 23)
+R CMD BATCH get_P.R
+
+# merge chr to genome file 
+p_chr2genome.sh
+
+# run manhattan and qq-plots (genome x 1)
+R CMD BATCH run_Manhattan.R
