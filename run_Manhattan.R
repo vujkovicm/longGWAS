e60933eef23f133e66de744c2e397b69ac505b64
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2015-02-20 11:50:46 -0500

    Update run_Manhattan.R

--- run_Manhattan.R
+++ run_Manhattan.R
@@ -5,20 +5,20 @@ library("qqman")
 setwd("/myDir")
 
 # import the output file as generated from get_P.R
-add<-read.table("myStudy.p", F)
-colnames(add)[1]<-"CHR"
-colnames(add)[2]<-"SNP"
-colnames(add)[3]<-"BP"
-colnames(add)[4]<-"P"
+df <- read.table("myStudy.p", F)
+colnames(df)[1] <- "CHR"
+colnames(df)[2] <- "SNP"
+colnames(df)[3] <- "BP"
+colnames(df)[4] <- "P"
 
 # manhattan plot
-png(file="pheno.png", width = 900, height = 350)
-manhattan(add, main = "Phenotype Info", ylim = c(0, 10), cex = 0.6, 
+png(file = "pheno.png", width = 900, height = 350)
+manhattan(df, main = "Phenotype Info", ylim = c(0, 10), cex = 0.6, 
     cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = T, genomewideline = T, 
     chrlabs = c(1:20, "P", "Q"))
 dev.off()
 
 # qq-plot
-png(file="pheno-qq.png", width = 350, height = 350)
-qq(add$P, main = "Q-Q plot: Phenotype ", xlim = c(0, 7), ylim = c(0, 12), pch = 18, col = "blue4", cex = 1.5, las = 1)
+png(file = "pheno-qq.png", width = 350, height = 350)
+qq(df$P, main = "Q-Q plot: Phenotype ", xlim = c(0, 7), ylim = c(0, 12), pch = 18, col = "blue4", cex = 1.5, las = 1)
 dev.off()
